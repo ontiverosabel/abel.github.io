a69b0ab62eeb64ea324833a0d043d844e16f24f5
Abel <ontiverosabel@gmail.com>
2023-12-15 18:35:26 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -25,8 +25,6 @@ function handleRadioChange() {
 }
 
 function handleUppercase() {
-      alert("BoringBetty is selected");
-
   var txt = document.getElementById("text");
   var upperCase = txt.value.toUpperCase();
   var sentences = upperCase.split('.'); 
