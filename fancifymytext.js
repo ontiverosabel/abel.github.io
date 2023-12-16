ee8aab0701b5bb16e91446dd14481d18ef3190ec
Abel <ontiverosabel@gmail.com>
2023-12-15 18:08:17 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,3 +1,6 @@
-function hello() {
-  alert("Hello, world!");
+function handleClick() {
+  var txt = document.getElementById("text");
+
+  // Change the font size to 24pt
+  txt.style.fontSize = "24pt";
 }
