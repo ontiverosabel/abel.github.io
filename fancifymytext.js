76b817f0194bc39667de69acda04db488d713554
Abel <ontiverosabel@gmail.com>
2023-12-15 18:20:57 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -2,5 +2,27 @@ function handleClick() {
   var txt = document.getElementById("text");
 
   // Change the font size to 24pt
-  txt.style.fontSize = "64pt";
+  txt.style.fontSize = "24pt";
+}
+function handleRadioChange() {
+  var fancyShmancyRadio = document.getElementById("fancyShmancyRadio");
+  var boringBettyRadio = document.getElementById("boringBettyRadio");
+  var textarea = document.getElementById("text");
+
+  if (fancyShmancyRadio.checked) {
+    alert("FancyShmancy is selected");
+    textarea.style.fontWeight = "bold";
+    textarea.style.textDecoration = "underline";
+    textarea.style.color = "blue";
+ 
+  
+  } else if (boringBettyRadio.checked) {
+    alert("BoringBetty is selected");
+    textarea.style.fontWeight = "normal";
+    textarea.style.textDecoration = "none";
+    textarea.style.color = "black";
+  }
+
+
+  
 }
